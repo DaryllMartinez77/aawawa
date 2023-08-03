0b759599556cc5c8c5cc975f4f3f0addb84435ac
Rizky Abdul Hafidz <113990700+DaryllMartinez77@users.noreply.github.com>
2023-08-03 13:06:00 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+x = 10;
+y = 5;
+
+# penjumlahan
+print("Penjumlahan : ", x + y);
+
+#pengurangan
+print("Pengurangan : ", x - y);
+
+#perkalian
+print("Perkalian : ", x * y);
+
+#pembagian
+print("Pembagian : ", x / y);
+
